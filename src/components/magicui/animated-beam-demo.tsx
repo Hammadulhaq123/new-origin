e4f12a0508816ff2be2341e1cf33efe58a9944da
JackAnderson01 <hammad.haq@launchbox.pk>
2025-03-13 22:50:52 +0500

    test

--- src/components/magicui/animated-beam-demo.tsx
+++ src/components/magicui/animated-beam-demo.tsx
@@ -4,7 +4,6 @@ export default function MeteorDemo() {
   return (
     <div className=" flex h-full w-full flex-col items-center justify-center   ">
       <Meteors number={50} />
-      {/*  */}
     </div>
   );
 }
